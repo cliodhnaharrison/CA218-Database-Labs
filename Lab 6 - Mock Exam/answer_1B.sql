d3b59c96d2d0e5e20c2e6193eb9f2bec082ebc27
Cliodhna Harrison <cliodhnaharrison@gmail.com>
2019-11-14 14:10:17 +0000

    Add missing part of lab 6 question 1B

--- Lab 6 - Mock Exam/answer_1B.sql
+++ Lab 6 - Mock Exam/answer_1B.sql
@@ -1,4 +1,4 @@
 SELECT f.title, c.name
 FROM sakila.film f, sakila.film_category fc, sakila.category c
-WHERE f.film_id = fc.film_id AND fc.category_id = c.category_id
+WHERE f.film_id = fc.film_id AND fc.category_id = c.category_id AND f.title LIKE "A%"
 ORDER BY title;
